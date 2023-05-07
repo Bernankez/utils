bcec27869a019876de165c763aa58b2a943cad1b
cole <bernankez@qq.com>
2023-05-08 00:29:48 +0800

    test: fix `resolve` test

--- test/resolve.spec.ts
+++ test/resolve.spec.ts
@@ -1,16 +1,20 @@
 import { describe, expect, it } from "vitest";
-import { resolvePath } from "../src";
+import { getPlatform, resolvePath } from "../src";
 
-const url = "file:///Users/username/Public/Projects/utils/src/resolve.ts";
+const isWindows = getPlatform() === "Win32";
+
+const url = isWindows ? "file:///C:/Projects/utils/src/resolve.ts" : "file:///Users/username/Public/Projects/utils/src/resolve.ts";
+const filenameUrl = isWindows ? "C:\\Projects\\utils\\src\\resolve.ts" : "/Users/username/Public/Projects/utils/src/resolve.ts";
+const dirnameUrl = isWindows ? "C:\\Projects\\utils\\src" : "/Users/username/Public/Projects/utils/src";
 
 const { __dirname, __filename } = resolvePath(url);
 
 describe("resolvePath", () => {
   it("check __filename", () => {
-    expect(__filename).toBe("/Users/username/Public/Projects/utils/src/resolve.ts");
+    expect(__filename).toBe(filenameUrl);
   });
 
   it("check __dirname", () => {
-    expect(__dirname).toBe("/Users/username/Public/Projects/utils/src");
+    expect(__dirname).toBe(dirnameUrl);
   });
 });
