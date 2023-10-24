517d422442431561eddf63d5bbaa9679ae1f0d13
cole <bernankeic@gmail.com>
2023-10-24 09:37:04 +0800

    docs: optimize style

--- .vitepress/components/FunctionList.vue
+++ .vitepress/components/FunctionList.vue
@@ -5,7 +5,7 @@
         {{ category }}
       </div>
       <div class="m-l-3 m-t-1 flex flex-gap-2">
-        <a v-for="func in functionsWithCategory[category]" :key="func.name" class="w-fit rounded-1 bg-code p-x-2 p-y-.5 font-400! text-default! decoration-none!" :href="func.url.doc.replace(/https?:\/\/utils\.keke\.cc\//g, '/')">
+        <a v-for="func in functionsWithCategory[category]" :key="func.name" class="w-fit rounded-1 bg-code p-x-2 p-y-.5 hover:bg-rose-50 font-400! text-default! decoration-none! transition! dark:hover:bg-dark-100" :href="func.url.doc.replace(/https?:\/\/utils\.keke\.cc\//g, '/')">
           {{ func.name }}
         </a>
       </div>
